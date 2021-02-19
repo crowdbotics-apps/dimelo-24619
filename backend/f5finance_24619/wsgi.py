c74b506d7cbe85c46bdd1172c41f95e0cc0d9cca
Crowdbotics <team@crowdbotics.com>
2021-02-19 15:32:18 +0000

    Update models

--- backend/f5finance_24619/wsgi.py
+++ backend/f5finance_24619/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'f5finance_24619.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "f5finance_24619.settings")
 
 application = get_wsgi_application()
